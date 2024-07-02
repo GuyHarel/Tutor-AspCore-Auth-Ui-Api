b84f3ee292ddaa55998beabaff61f9abd1e58907
GuyHarel <guyharel13@gmail.com>
2024-07-02 10:50:10 -0400

    BasicAuthenticationHandler

---
+++ AspNetCoreRazor/Security/Authentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace AspNetCoreRazor.Security.Authentication
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, 
+            ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
+            var username = credentials[0];
+            var password = credentials[1];
+
+            // Vérifiez les informations d'identification ici
+            if (username != "test" || password != "password")
+            {
+                return AuthenticateResult.Fail("Invalid Username or Password");
+            }
+
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, username),
+            new Claim(ClaimTypes.Email, "test@example.com"),
+            new Claim(ClaimTypes.Role, "User")
+        };
+
+            var claimsIdentity = new ClaimsIdentity(claims, Scheme.Name);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            var authTicket = new AuthenticationTicket(claimsPrincipal, Scheme.Name);
+
+            return await Task.FromResult(AuthenticateResult.Success(authTicket));
+        }
+
+        protected override Task HandleChallengeAsync(AuthenticationProperties properties)
+        {
+            Response.StatusCode = 401;
+            Response.Headers["WWW-Authenticate"] = "Basic realm=\"example.com\"";
+            return Response.WriteAsync("Unauthorized");
+        }
+
+        protected override Task HandleForbiddenAsync(AuthenticationProperties properties)
+        {
+            Response.StatusCode = 403;
+            return Response.WriteAsync("Forbidden");
+        }
+
+    }
+}
